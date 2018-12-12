34a44e795794e534c22ba332e533fc4aec479554
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2018-12-12 17:04:54 +0200

    foolproofing

--- app.py
+++ app.py
@@ -32,20 +32,23 @@ def test():
 
 
 if __name__ == '__main__':
+  print("Backend starting")
   load_dotenv()
   if not os.path.isdir("./model"):
     print("NO MODEL VOLUME")
     exit(1)
   debug = os.getenv('ENV') == 'development'
   global model
+  boolean_print = False
   while True:
     try:
       model = pickle.load(open('./model/kurkkumopotin.sav', 'rb'))
       break
     except:
-      print("Model not found. Waiting 15 seconds to retry...")
-      time.sleep(15)
+      if not boolean_print:
+        print("No 'kurkkumopotin.sav' model in the model volume. Waiting for training service to provide one.")
+        boolean_print = True
+      time.sleep(5)
       continue
-
 
   app.run("0.0.0.0", port=5000, debug=debug)
