17a6d6b386fb3c2b59108477c54f9eef608fa1a3
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2018-12-12 17:06:31 +0200

    doesnt train if model already exists

--- train.py
+++ train.py
@@ -41,6 +41,7 @@ def train(X, y):
     model.fit(x=X_train, y=y_train, batch_size=128, epochs=5, verbose=1)
     points = model.evaluate(X_test, y_test)
     pickle.dump(model, open('./model/kurkkumopotin.sav', 'wb'))
+    return
 
 def get_images(prefix, url):
     imgs = [] 
@@ -60,32 +61,38 @@ def get_images(prefix, url):
 
 if __name__ == "__main__":
     K.clear_session()
+    try:
+        pickle.load(open("./model/kurkkumopotin.sav", "rb"))
+        print("Model already exists at './model/kurkkumopotin.sav'")
+    except:
+        try:
+            cucumbers = pd.read_csv("./data/cucumber.csv")
+            mopeds = pd.read_csv('./data/moped.csv')
+        except:
+            print("Cant find 'data' volume with csv:s for image download ")
+            exit(1)
 
- 
-    cucumbers = pd.read_csv("./data/cucumber.csv")
-    mopeds = pd.read_csv('./data/moped.csv')
+        p = Pool(100)
+
+        print("gathering cucumbers...")
+        func = partial(get_images, "cucumber_")
+        cucumber_imgs = p.map(func, cucumbers["url"])
+        print("gathering mopeds...")
+        func = partial(get_images, "moped_")
+        moped_imgs = p.map(func, mopeds["url"])
+        p.close()
+        cucumbers = pd.DataFrame({"uri": cucumber_imgs, "y": 0})
+        cucumbers.to_csv("./data/cucumbers2.csv")
+        mopeds = pd.DataFrame({"uri": moped_imgs, "y": 1})
+        mopeds.to_csv("./data/mopeds2.csv")
+
+        merged = pd.concat([pd.read_csv("./data/cucumbers2.csv"), pd.read_csv("./data/mopeds2.csv")])
+        merged = merged.dropna()
+        X = merged.drop(["y"], axis=1)
+        y = merged.drop(["uri"], axis=1)
+
+        train(X, y)
+        exit(0)
+
     
-    p = Pool(100)
-    
-    print("gathering cucumbers...")
-    func = partial(get_images, "cucumber_")
-    cucumber_imgs = p.map(func, cucumbers["url"])
-    print("gathering mopeds...")
-    func = partial(get_images, "moped_")
-    moped_imgs = p.map(func, mopeds["url"])
-    p.close()
-    cucumbers = pd.DataFrame({"uri": cucumber_imgs, "y": 0})
-    cucumbers.to_csv("./data/cucumbers2.csv")
-    mopeds = pd.DataFrame({"uri": moped_imgs, "y": 1})
-    mopeds.to_csv("./data/mopeds2.csv")
-
-    merged = pd.concat([pd.read_csv("./data/cucumbers2.csv"), pd.read_csv("./data/mopeds2.csv")])
-    merged = merged.dropna()
-    X = merged.drop(["y"], axis=1)
-    y = merged.drop(["uri"], axis=1)
-
-    train(X, y)
-
-    
-  
 
