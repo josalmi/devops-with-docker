c6a7bbfd67a19d96c15f03fd4995f80c56b2416f
Jakousa <jakousa@cs.helsinki.fi>
2019-01-11 19:40:57 +0200

    a marginally better error handling

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,10 +3,15 @@ import Sequelize from 'sequelize'
 let sequelize
 
 if (DB.username && DB.password) {
-    sequelize = new Sequelize(DB.database, DB.username, DB.password, {
-        host: DB.host,
-        dialect: 'postgres'
-    });
+    try {
+        sequelize = new Sequelize(DB.database, DB.username, DB.password, {
+            host: DB.host,
+            dialect: 'postgres'
+        });
+    } catch (err) {
+        console.log(err, 'Exiting before making connection')
+        process.exit(1);
+    }
 }
 
 export default sequelize
--- server/models/message.js
+++ server/models/message.js
@@ -3,15 +3,25 @@ import sequelize from '../database/connection'
 
 let Message
 
+const sync = async sequelize => {
+    try {
+        await sequelize.sync()
+    } catch (err) {
+        console.error(err)
+        console.log('Errored before messages were initialized in database')
+        process.exit(1)
+    }
+}
+
 if (sequelize) {
     Message = sequelize.define('message', {
         body: Sequelize.STRING,
     }, {
-            tablename: 'messages',
-            timestamps: true
-        });
+        tablename: 'messages',
+        timestamps: true
+    });
 
-    sequelize.sync()
+    sync(sequelize)
 }
 
 export default Message
