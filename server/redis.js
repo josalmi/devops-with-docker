81d452c883e019ee0d9ad36548597f2f017ed536
Kousa <jami.kousa@helsinki.fi>
2019-03-25 14:34:27 +0200

    redis improved

--- server/redis.js
+++ server/redis.js
@@ -4,14 +4,18 @@ const { promisify } = require('util');
 
 let getAsync = async () => { }
 let setAsync = async () => { }
-try {
-  if (REDIS) {
+if (REDIS) {
+  try {
+    console.log('Redis connection, initating..')
     const client = redis.createClient(REDIS_PORT, REDIS)
     getAsync = promisify(client.get).bind(client);
     setAsync = promisify(client.set).bind(client);
+    console.log('Trying to set cache')
+    setAsync("slow", "pong")
+    console.log('Cache set successfully')
+  } catch (err) {
+    console.log('Problems connecting to redis, make sure you have the correct port!')
   }
-} catch (err) {
-  console.log('Problems connecting to redis, make sure you have the correct port')
 }
 module.exports = {
   getAsync,
